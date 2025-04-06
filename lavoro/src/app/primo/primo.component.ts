6d59ff0dbf935122eb7852b7f1e79a2a6bb219b5
MucaErind Muca <muca.erind@iisgalvanimi.edu.it>
2025-04-06 16:03:31 +0000

    secondo passo

---
+++ lavoro/src/app/primo/primo.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core'; //Molto simile alle import in Java
+
+@Component({
+  selector: 'app-primo',
+  templateUrl: './primo.component.html',
+  styleUrls: ['./primo.component.css']
+})
+export class primo implements OnInit {
+  constructor() { }
+  ngOnInit() {
+  }
+}
